acd3225f77c3e69bb8867aef4dde484999d554de
emilys28 <esims28.97@gmail.com>
2024-01-14 19:23:54 -0500

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -42,9 +42,9 @@ def welcome():
     return (
         f"Welcome to the .. API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
         f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
     )
